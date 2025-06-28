ec201b94bfa5a6f3be984c4ce24f31ecd5fb5123
Ray <jacktheamazinghd@gmail.com>
2025-06-28 16:50:37 +0100

    Added extra.c

---
+++ extra.c
@@ -0,0 +1,19 @@
+// ANSI escape shit
+
+#define RED "\033[1;91m"
+#define WHITE "\033[1;37m"
+#define GREEN "\033[1;92m"
+#define ERROR "\033[1;37m[\033[1;91m-\033[1;37m]\033[1;91m"
+#define SUCCESS "\033[1;37m[\033[1;92m+\033[1;37m]\033[1;92m"
+
+
+// Functions :3
+
+unsigned int compare(char *s1, char *s2){
+    unsigned int index=0;
+    while(*(s1+index)!='\0' && *(s2+index)!='\0'){
+        if(*(s1+index)!=*(s2+index)) return 0;
+        index++;
+    }
+    return 1;
+}
