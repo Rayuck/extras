56a3a551b19cb657ab6e632f65ba3c31e423355f
Rayuck <Rayuck@github.com>
2025-06-30 15:29:42 +0100

    Added WARNING in extra.c

--- extra.c
+++ extra.c
@@ -4,6 +4,7 @@
 #define WHITE "\033[1;37m"
 #define GREEN "\033[1;92m"
 #define DEFAULT "\033[0m"
+#define WARNING "\033[1;37m[\033[1;93m~\033[1;37m"
 #define ERROR "\033[1;37m[\033[1;91m-\033[1;37m]\033[1;91m"
 #define SUCCESS "\033[1;37m[\033[1;92m+\033[1;37m]\033[1;92m"
 
