ca4253991dc87c530f87c7f6b401f23d5802dfa3
Rayuck <Rayuck@github.com>
2025-06-30 15:33:18 +0100

    Fixed WARNING & added YELLOW

--- extra.c
+++ extra.c
@@ -3,8 +3,9 @@
 #define RED "\033[1;91m"
 #define WHITE "\033[1;37m"
 #define GREEN "\033[1;92m"
+#define YELLOW "\033[1;93m"
 #define DEFAULT "\033[0m"
-#define WARNING "\033[1;37m[\033[1;93m~\033[1;37m"
+#define WARNING "\033[1;37m[\033[1;93m~\033[1;37m]\033[1;93m"
 #define ERROR "\033[1;37m[\033[1;91m-\033[1;37m]\033[1;91m"
 #define SUCCESS "\033[1;37m[\033[1;92m+\033[1;37m]\033[1;92m"
 
